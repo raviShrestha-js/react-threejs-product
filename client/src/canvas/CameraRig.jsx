cc0d67cc5b7d6471d2158ba6067132ddf888b275
DevRavi <35264417+raviShrestha-js@users.noreply.github.com>
2023-04-05 16:48:35 +1000

    Tabs and Color Picker Completed

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -17,7 +17,7 @@ const CameraRig = ({ children }) => {
     let targetPosition = [-0.4, 0, 2];
     if (snap.intro) {
       if (isBreakpoint) targetPosition = [0, 0, 2];
-      if (isMobile) targetPosition = [o, 0.2, 2.5];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
     } else {
       if (isMobile) targetPosition = [0, 0, 2.5];
       else targetPosition = [0, 0, 2];
@@ -30,7 +30,7 @@ const CameraRig = ({ children }) => {
 
     easing.dampE(
       group.current.rotation,
-      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      [state.pointer.y / 5, -state.pointer.x / 2, 0],
       0.25,
       delta
     );
--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { SketchPicker } from "react-color";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
 
 const ColorPicker = () => {
-  return <div>ColorPicker</div>;
+  const snap = useSnapshot(state);
+
+  return (
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        // presetColors={["#0000ff", "#EFBD4E"]}
+        onChange={(color) => (state.color = color.hex)}
+      />
+    </div>
+  );
 };
 
 export default ColorPicker;
